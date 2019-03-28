faaf431563e79905786e966386be92c08767adc0
brian haltom <brianwh87@gmail.com>
2019-03-28 12:23:10 -0700

    add styling to show template

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 # Articles Model
 class Article < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 500000 }
 end
