48a13e4521cbad8018fbf34b628ed14098f21e15
brian haltom <brianwh87@gmail.com>
2019-03-28 22:06:44 -0700

    Signup users from UI

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+# Users Controller
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Hey there, #{@user.username}! Welcome to Stylts"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
