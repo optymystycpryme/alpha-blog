69957c9ebf1a0fb6cbf1aef4cb2335ab03fa8fc6
brian haltom <brianwh87@gmail.com>
2019-03-30 22:21:03 -0700

    Add categories model and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_30_015742) do
+ActiveRecord::Schema.define(version: 2019_03_31_034127) do
 
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer "user_id"
+  create_table 'articles', force: :cascade do |t|
+    t.string 'title'
+    t.text 'description'
+    t.datetime 'created_at'
+    t.datetime 'updated_at'
+    t.integer 'user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "password_digest"
-    t.boolean "admin", default: false
+  create_table 'categories', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+  end
+
+  create_table 'users', force: :cascade do |t|
+    t.string 'username'
+    t.string 'email'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'password_digest'
+    t.boolean 'admin', default: false
   end
 
 end
