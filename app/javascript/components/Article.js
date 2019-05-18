2a3a8c91bf9910c375d7ed90c2d9d893bd01d60b
brian haltom <brianwh87@gmail.com>
2019-05-17 20:55:38 -0700

    Integrated date-fns for time stamping

--- app/javascript/components/Article.js
+++ app/javascript/components/Article.js
@@ -1,16 +1,24 @@
 import React from "react"
 import PropTypes from "prop-types"
+import distanceInWordsToNow from "date-fns/distance_in_words_to_now";
 class Article extends React.Component {
   render () {
+    const updatedAt = distanceInWordsToNow(
+      new Date(this.props.updated_at)
+    )
     return (
       <React.Fragment>
-        <div className="article-title"><a href={this.props.path}>{this.props.title}</a></div>
+        <div className="article-title">
+          <a href={this.props.path}>{this.props.title}</a>
+        </div>
         <div className="article-body">
           {this.props.description}
           <div className="article-meta-details">
             <small>
               Written by: {this.props.author}
-              <p>Updated: {this.props.updated_at} ago.</p>
+              <p>
+                Updated: {updatedAt} ago.
+              </p>
             </small>
           </div>
         </div>
